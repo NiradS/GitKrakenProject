e6ebbac71aa41ea294c697671db78c99bd6042af
Pranav Komarla <10020547@sbstudents.org>
2022-03-16 11:55:02 -0400

    Combined both runners

--- Runner.java
+++ Runner.java
@@ -2,10 +2,20 @@ interface perfectSquare {
   boolean run(int num);
 }
 
-<<<<<<< HEAD
+interface PrintQuotient
+{
+	void run(float x, float y);
+}
+
 public class Runner {
   public static void main(String[] args)
   {
+	  PrintQuotient printQuotient = (x, y) -> {float quotient a/b;
+	  		quotient = Math.round(quotient*1000.0f)/1000.0f;
+	  		System.out.printf("Quotient: %s%n", quotient); };
+
+	  printQuotient.run(2, 7);
+
       perfectSquare isItSquare = x -> (Math.sqrt(x) == Math.round(Math.sqrt(x)));
       perfectChecker(35,isItSquare);
   }
@@ -14,22 +24,4 @@ public class Runner {
     boolean result = perfect.run(num);
     System.out.println(result);
   }
-=======
-public class Runner
-{
-
-	public static void main(String[]args)
-	{
-		PrintQuotient printQuotient = (x, y) -> {float quotient a/b;
-		quotient = Math.round(quotient*1000.0f)/1000.0f;
-		System.out.printf("Quotient: %s%n", quotient); };
-
-		printQuotient.run(2, 7);
-
-
-	}
-
-
-
->>>>>>> Print_Quotient
 }
