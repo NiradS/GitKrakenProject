b72f698918d00b7d8d692a911cdb79255eb04686
Pranav Komarla <10020547@sbstudents.org>
2022-03-16 11:50:44 -0400

    Runner and Interface for print quotient functionality created

---
+++ Runner.java
@@ -0,0 +1,21 @@
+interface perfectSquare {
+  boolean run(int num);
+}
+
+public class Runner
+{
+
+	public static void main(String[]args)
+	{
+		PrintQuotient printQuotient = (x, y) -> {float quotient a/b;
+		quotient = Math.round(quotient*1000.0f)/1000.0f;
+		System.out.printf("Quotient: %s%n", quotient); };
+
+		printQuotient.run(2, 7);
+
+
+	}
+
+
+
+}
